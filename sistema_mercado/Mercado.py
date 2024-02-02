6ba78d3ebdfc83c7c291ca0cb84f9d570fa1b2da
CarolinaSanches24 <carolmosilva34@gmail.com>
2024-02-02 00:34:01 -0300

    feat: ajustes na logica para registro das transacoes

--- sistema_mercado/Mercado.py
+++ sistema_mercado/Mercado.py
@@ -1,12 +1,32 @@
 from datetime import datetime
-#Modelagem inicial, aberta a mudanças
-class Cliente():
-    def __init__(self, id_cliente, nome_cliente, telefone_cliente, endereco_cliente):
-        self._id_cliente = id_cliente
-        self._nome = nome_cliente
-        self._telefone = telefone_cliente
-        self._endereco = endereco_cliente
-        # aqui podemos implementar encapsulamento e deixar esse dados como private
+
+# Observaçoes falta adicionar mais coisas de POO e tratamento de erros
+# Por enquanto foi feita a modelagem 
+# e aplicado Herança
+class Pessoa :
+    def __init__(self, id, nome, telefone, endereco):
+        self._id= id;
+        self._nome = nome;
+        self._telefone = telefone;
+        self._endereco = endereco;
+        
+        @property
+        def id(self):
+            return self._id;
+        @property
+        def nome(self):
+            return self._nome;
+        
+        @property
+        def telefone (self):
+            return self._telefone;
+        @property
+        def endereco(self):
+            return self._endereco;
+        
+class Cliente(Pessoa):
+    def __init__(self, id, nome, telefone, endereco):
+        super().__init__(id, nome, telefone, endereco)
         
          #Método para exibir informações do cliente
     def __str__(self):
@@ -17,6 +37,19 @@ class Mercado():
         self.__lista_clientes = [];
         self.lista_produtos = [];
         self.__lista_transacoes = [];
+        
+    @property 
+    def lista_transacoes(self):
+        return self.__lista_transacoes;
+    
+    def vender_produto(self, produto, cliente, quantidade):
+        if quantidade > produto.quantidade_produtos:
+            print("Quantidade insuficiente de produtos.")
+            return
+        produto.quantidade_produtos -= quantidade
+        transacao = Transacao(cliente._id, cliente._nome, produto.nome, quantidade)
+        self.__lista_transacoes.append(transacao)
+        print(f"Compra realizada: {quantidade} unidades de {produto.nome}")
         
     def adicionar_cliente(self,cliente):
         self.__lista_clientes.append(cliente);
@@ -30,9 +63,6 @@ class Mercado():
     def mostrar_produtos(self):
         for produto in self.lista_produtos:
             print(produto)
-            
-    def adicionar_transacao (self, transacao):
-        self.__lista_transacoes.append(transacao);
         
     def mostrar_transacoes (self):
         for transacao in self.__lista_transacoes:
@@ -43,7 +73,7 @@ class Produto:
         self.id_produto = id_produto;
         self.nome = nome_produto
         self.categoria = []
-        self.quantidade_produtos = quantidade_produtos;
+        self._quantidade_produtos = quantidade_produtos;
         self.fornecedores =[]
     
     def adicionar_fornecedor(self, fornecedor):
@@ -52,48 +82,61 @@ class Produto:
     def adicionar_categoria(self, categoria):
         self.categoria.append(categoria)
         
+    @property
+    def quantidade_produtos(self):
+        return self._quantidade_produtos
+
+    @quantidade_produtos.setter
+    def quantidade_produtos(self, nova_quantidade):
+        self._quantidade_produtos = nova_quantidade
+    
+        
     def __str__(self):
-        return (f'ID:{self.id_produto}, Nome:{self.nome},'
+        return (
+                f'ID:{self.id_produto}, Nome:{self.nome},'
                 f' Quantidade:{self.quantidade_produtos}'
                 f' Categorias: {self.categoria}'
                 f' Fornecedores:{self.fornecedores}')
         
 class Transacao:
-    def __init__(self, id_cliente, nome_cliente, produtos):
+    def __init__(self, id_cliente, nome_cliente, produtos, quantidade):
         self._id_cliente = id_cliente;
         self._nome_cliente = nome_cliente;
         self._produtos = produtos
+        self._quantidade = quantidade
         self.data_da_compra = datetime.now();
     
     def __str__(self):
         data_formatada = self.data_da_compra.strftime('%d %m %Y %H:%M');
-        return (f'ID: {self._id_cliente}'
-                f' Nome Cliente : {self._nome_cliente}'
-                f' Produtos : {self._produtos}'
-                f' Data da Compra: {data_formatada}'
+        return (f'ID: {self._id_cliente}\n'
+                f' Nome Cliente : {self._nome_cliente}\n'
+                f' Produtos : {self._produtos}\n'
+                f' Quantidade:{self._quantidade}\n'
+                f' Data da Compra: {data_formatada}\n'
                 )
    
      
 mercado = Mercado();
 cliente1 = Cliente(1,"Clarice", 87879494, "Campina Grande")
-# print(cliente1)
+mercado.adicionar_cliente(cliente1);
 
 produto1 = Produto(1,"Arroz",50);
 produto1.adicionar_categoria("Carboidratos");
 produto1.adicionar_fornecedor("Comercial Norte Distribuição")
+mercado.adicionar_produto(produto1);
 
 produto2 = Produto(2,"Feijão",10);
 produto2.adicionar_categoria("Proteina");
 produto2.adicionar_fornecedor("Comercial Norte Distribuição")
+mercado.adicionar_produto(produto2);
 
-# ainda falta aprimorar a logica da transacao sintam-se a vontade para modificar
-produtos_comprados = [(1,5), (2,3)] # id_produto e #quantidade
-transacao1 = Transacao(1,"Clarice",produtos_comprados);
-mercado.adicionar_transacao(transacao1);
+mercado.vender_produto(produto1, cliente1, 10);
+mercado.vender_produto(produto2, cliente1, 5);
+mercado.vender_produto(produto2, cliente1, 10);
+#estoque do produto apos compras do cliente
+print(f'Estoque do produto\n {produto1}');
+print(f'Estoque do produto\n {produto2}');
+
 mercado.mostrar_transacoes();
-
-mercado.adicionar_cliente(cliente1);
 mercado.mostrar_clientes();
-
-mercado.adicionar_produto(produto1);
 mercado.mostrar_produtos();
