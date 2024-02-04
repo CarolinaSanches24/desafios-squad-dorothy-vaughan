3240b7349faa184496a547d68324dd14eb682211
maiharamoni <maiharamoni@gmail.com>
2024-02-04 10:12:35 -0300

    insercao da tabela de transacoes

--- banco_dados_mercado/tabela.py
+++ banco_dados_mercado/tabela.py
@@ -7,6 +7,8 @@ cursor.execute('CREATE TABLE clientes(id_cliente int primary key, nome_cliente v
 cursor.execute('CREATE TABLE produtos(id_produto int primary key, nome_produto varchar(100), categoria_produto varchar(50), qtde_produto int, fornecedor_produto varchar(100))')
 ##cursor.execute('CREATE TABLE transacao(dt_compra date, )
 
+# Criacao da tabela Transacoes
+cursor.execute('CREATE TABLE transacoes(id_transacao int primary key, dt_compra DATE, id_cliente INTEGER, FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente))')
 
 
 
