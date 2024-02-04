2d4c137e56d90ef1336af47ac79a0223fa1805e0
anafbarreto <karolbarreto10@gmail.com>
2024-02-04 10:42:09 -0300

    dados

--- banco_dados_mercado/tabela.py
+++ banco_dados_mercado/tabela.py
@@ -3,10 +3,16 @@ import sqlite3
 conexao = sqlite3.connect('mercado.bd')
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE clientes(id_cliente int primary key, nome_cliente varchar(100), telefone_cliente int, endereco_cliente varchar(100))')
-cursor.execute('CREATE TABLE produtos(id_produto int primary key, nome_produto varchar(100), categoria_produto varchar(50), qtde_produto int, fornecedor_produto varchar(100))')
+##cursor.execute('CREATE TABLE clientes(id_cliente int primary key not null, nome_cliente varchar(100) not null, telefone_cliente int, endereco_cliente varchar(100) not null)')
+##cursor.execute('CREATE TABLE produtos(id_produto int primary key not null, nome_produto varchar(100) not null, categoria_produto varchar(50), qtde_produto int not null, fornecedor_produto varchar(100) not null)')
 ##cursor.execute('CREATE TABLE transacao(dt_compra date, )
 
+cursor.execute('insert into clientes(id_cliente, nome_cliente, telefone_cliente, endereco_cliente) values (1, "Ana", 12344321, "rua flores") ')
+cursor.execute('insert into clientes(id_cliente, nome_cliente, telefone_cliente, endereco_cliente) values (2, "Maite", 12344377, "rua jardim")')
+cursor.execute('insert into clientes(id_cliente, nome_cliente, telefone_cliente, endereco_cliente) values (3, "Simone", 34544321, "rua rosas")')
+cursor.execute('insert into clientes(id_cliente, nome_cliente, telefone_cliente, endereco_cliente) values (4, "Jane", 12355321, "rua margaridas")')
+cursor.execute('insert into clientes(id_cliente, nome_cliente, telefone_cliente, endereco_cliente) values (5, "Isadora", 12346661, "rua tulipa")')
+cursor.execute('insert into produtos(id_produto, nome_produto, categoria_produto, qtde_produto, fornecedor_produto) values (1, "sabao limpinho", "limpeza", 10, "triex")')
 
 
 
